2b2b4ab7ba8612ed8a52d46fe27932aaaa7c9d87
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-16 18:02:50 +0200

    :wrench: v2  :wrench:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cryptoData, setCryptoData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20");
+    };
+  })
   return (
     <div className="App">
      
